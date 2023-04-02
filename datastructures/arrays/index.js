2c5548aa8539af526f92644b73d65407c9f3c24b
JemilCodes <jemiluishaq277@gmail.com>
2023-04-02 10:22:18 +0100

    created a class for making an array

---
+++ datastructures/arrays/index.js
@@ -0,0 +1,14 @@
+class MyArray {
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
+}
+
+const newArray = new MyArray();
+// newArray.push("hello world");
+// newArray.pop();
+// newArray.push("hello world2");
+// newArray.push("hello world3");
+// newArray.delete(1);
+console.log(newArray.data);
