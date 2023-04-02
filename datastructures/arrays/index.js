ce3e173ce947f356d2d868b0376bcd5d5522f655
JemilCodes <jemiluishaq277@gmail.com>
2023-04-02 10:36:25 +0100

    added a delete method

--- datastructures/arrays/index.js
+++ datastructures/arrays/index.js
@@ -21,13 +21,17 @@ class MyArray {
     delete this.data[this.length - 1];
     this.length--;
   }
+  delete(index) {
+    this.shiftItem(index);
+    return this.data[index];
+  }
 }
 
 const newArray = new MyArray();
 newArray.push("hello world");
 newArray.getItem(0);
 newArray.pop();
-// newArray.push("hello world2");
-// newArray.push("hello world3");
-// newArray.delete(1);
+newArray.push("hello world2");
+newArray.push("hello world3");
+newArray.delete(1);
 console.log(newArray.data);
