7bc729242fbbd6ae21f1ecb829acbd5218f04917
JemilCodes <jemiluishaq277@gmail.com>
2023-04-05 00:36:28 +0100

    added a hash function

--- datastructures/hash-tables/index.js
+++ datastructures/hash-tables/index.js
@@ -2,4 +2,11 @@ class myClass {
   constructor(size) {
     this.data = new Array(size);
   }
+  _hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+    }
+    return hash;
+  }
 }
