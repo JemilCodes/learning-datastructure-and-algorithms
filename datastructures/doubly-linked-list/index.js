403ad306eefdbd149478b882809d0ffac83a9aaf
JemilCodes <jemiluishaq277@gmail.com>
2023-04-11 23:05:52 +0100

    added a class for doubly-linked-list

--- datastructures/doubly-linked-list/index.js
+++ datastructures/doubly-linked-list/index.js
@@ -1,20 +1,20 @@
-// class myClass {
-//   constructor(value) {
-//     this.head = {
-//       value,
-//       next: null,
-//       prev: null,
-//     };
-//     this.tail = this.head;
-//     this.length = 1;
-//   }
-// }
+class myClass {
+  constructor(value) {
+    this.head = {
+      value,
+      next: null,
+      prev: null,
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+}
 
-// const linkedList = new myClass(5);
-// // linkedList.prepend(10);
-// // linkedList.append(15);
-// // linkedList.append(20);
-// // linkedList.insert(2, 25);
-// // linkedList.remove(3);
-// // // linkedList.reverse();
-// // linkedList.printList();
+const linkedList = new myClass(5);
+// linkedList.prepend(10);
+// linkedList.append(15);
+// linkedList.append(20);
+// linkedList.insert(2, 25);
+// linkedList.remove(3);
+// // linkedList.reverse();
+// linkedList.printList();
