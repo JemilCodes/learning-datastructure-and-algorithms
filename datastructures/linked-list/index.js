55e491d15bca5107e9f0e94329409e2d8420722c
JemilCodes <jemiluishaq277@gmail.com>
2023-04-11 15:38:26 +0100

    made corrections

--- datastructures/linked-list/index.js
+++ datastructures/linked-list/index.js
@@ -10,10 +10,10 @@ class myClass {
 }
 
 const linkedList = new myClass(5);
-linkedList.append(10);
-linkedList.append(15);
-linkedList.append(20);
+// linkedList.append(10);
+// linkedList.append(15);
+// linkedList.append(20);
 // linkedList.insert(2, 25);
 // linkedList.remove(3);
-linkedList.reverse()
-linkedList.printList();
+// linkedList.reverse()
+// linkedList.printList();
