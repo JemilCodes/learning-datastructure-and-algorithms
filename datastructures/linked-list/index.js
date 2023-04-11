af9c3d34a49a48929954ad8b44da1f5d3d5f67f7
JemilCodes <jemiluishaq277@gmail.com>
2023-04-11 15:40:19 +0100

    added a prepend function

--- datastructures/linked-list/index.js
+++ datastructures/linked-list/index.js
@@ -7,10 +7,19 @@ class myClass {
     this.tail = this.head;
     this.length = 1;
   }
+  prepend(value) {
+    let newNode = {
+      value,
+      next: this.head,
+    };
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
 }
 
 const linkedList = new myClass(5);
-// linkedList.append(10);
+linkedList.prepend(10)
 // linkedList.append(15);
 // linkedList.append(20);
 // linkedList.insert(2, 25);
