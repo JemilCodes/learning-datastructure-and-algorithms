4a55034e23876fd8f36f779e89b5a38d353408d4
JemilCodes <jemiluishaq277@gmail.com>
2023-04-29 13:26:41 +0100

    added an insert method

---
+++ datastructures/BST/index.js
@@ -0,0 +1,64 @@
+class Node {
+  constructor(value) {
+    this.left = null;
+    this.right = null;
+    this.value = value;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    const newNode = new Node(value);
+
+    if (!this.root) {
+      this.root = newNode;
+      return;
+    }
+
+    let root = this.root;
+
+    while (true) {
+      if (value < root.value) {
+        if (!root.left) {
+          root.left = newNode;
+          break;
+        }
+        root = root.left;
+      } else if (value >= root.value) {
+        if (!root.right) {
+          root.right = newNode;
+          break;
+        }
+        root = root.right;
+      }
+    }
+  }
+  lookup(value) {
+    //Code here
+  }
+  // remove
+}
+
+const tree = new BinarySearchTree();
+tree.insert(9);
+tree.insert(4);
+tree.insert(6);
+tree.insert(20);
+tree.insert(170);
+tree.insert(15);
+tree.insert(1);
+console.log(JSON.stringify(traverse(tree.root)));
+
+//     9
+//  4     20
+//1  6  15  170
+
+function traverse(node) {
+  const tree = { value: node.value };
+  tree.left = node.left === null ? null : traverse(node.left);
+  tree.right = node.right === null ? null : traverse(node.right);
+  return tree;
+}
